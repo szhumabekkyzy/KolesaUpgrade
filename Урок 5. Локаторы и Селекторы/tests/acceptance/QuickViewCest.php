7976d4590a0c9a3ce1cf7ddb0ff9a53f0dada019
Sholpan Zhumabekkyzy <szhumabekkyzy@gmail.com>
2021-05-27 11:22:04 +0600

    Added CSS and Xpath variables

--- Урок 5. Локаторы и Селекторы/tests/acceptance/QuickViewCest.php
+++ Урок 5. Локаторы и Селекторы/tests/acceptance/QuickViewCest.php
@@ -7,7 +7,17 @@ class QuickViewCest
      */
     public function checkSearchInsideQuickView(AcceptanceTester $I)
     {
-        
+        $searchProductToCSS = '#homefeatured > li:nth-child(2)';
+        $searchProductXPath = '//ul[@id="homefeatured"]/li[2]';
+        $moveMouseOverCSS = '#homefeatured > li:nth-child(2)';
+        $moveMouseOverXPath = '//ul[@id="homefeatured"]/li[2]';
+        $quickViewButtonCSS = '#homefeatured > li:nth-child(2) a.quick-view';
+        $quickViewButtonXPath = '//ul[@id="homefeatured"]/li[2]//a[@class="quick-view"]'; 
+        $IFrameCSS = '.fancybox-iframe';
+        $IFrameXPath = '//iframe[@class="fancybox-iframe"]';
+        $waitForTextCSS = 'h1[itemprop="name"]';
+        $waitForTextXPath = '//h1[@itemprop="name"]';
+
         $I->amOnPage('');
         $I->scrollTo('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
         $I->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img');
--- Урок 5. Локаторы и Селекторы/tests/functional/SearchCest.php
+++ Урок 5. Локаторы и Селекторы/tests/functional/SearchCest.php
@@ -7,6 +7,13 @@ class   SearchCest
      */
     public function checkSearchByText(FunctionalTester $I)
     {
+        $fillFieldCSS = '#search_query_top';
+        $fillFieldXPath = '//input[@id="search_query_top"]';
+        $clickCSS = 'button[name="submit_search"]';
+        $clickXPath = '//*["@name="submit_search"]/button';
+        $seeNumberOfElementsCSS = '.product-container';
+        $seeNumberOfElementsXPath = '//ul[@class="product_list grid row"]/li';
+
         $I->amOnPage('');
         $I->seeElement('#search_query_top');
         $I->fillField('#search_query_top','Printed dress');
