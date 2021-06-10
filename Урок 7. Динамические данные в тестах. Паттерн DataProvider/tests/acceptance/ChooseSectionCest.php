ecb3a1010b6594931dbebc70556d9970fa8c4c95
Sholpan <84121599+szhumabekkyzy@users.noreply.github.com>
2021-06-10 14:29:10 +0600

    Update ChooseSectionCest.php

--- Урок 7. Динамические данные в тестах. Паттерн DataProvider/tests/acceptance/ChooseSectionCest.php
+++ Урок 7. Динамические данные в тестах. Паттерн DataProvider/tests/acceptance/ChooseSectionCest.php
@@ -14,8 +14,8 @@ class ChooseSeactionCest
 {
     /**
      * Тест на выбор 2-х случайных разделов из меню категории
-     * @group test
-     * @param Example $data
+     * @group        test
+     * @param        Example $data
      * @dataProvider getDataForSearchSectionByCategory
      */
     public function searchSectionByCategory (\AcceptanceTester $I, Example $data)
@@ -30,12 +30,12 @@ class ChooseSeactionCest
     protected function getDataForSearchSectionByCategory () 
     {
         $sections = [
-            ['nameSection' => 'Разработка', 'sectionUrl' => 'flows/develop/'],
+            ['nameSection' => 'Разработка',         'sectionUrl' => 'flows/develop/'],
             ['nameSection' => 'Администрирование' , 'sectionUrl' => 'flows/admin/'],
-            ['nameSection' => 'Дизайн', 'sectionUrl' => 'flows/design/'],
-            ['nameSection' => 'Менеджмент' , 'sectionUrl' => 'flows/management/'],
-            ['nameSection' => 'Маркетинг', 'sectionUrl' => 'flows/marketing/'],
-            ['nameSection' => 'Научпоп' , 'sectionUrl' => 'flows/popsci/']
+            ['nameSection' => 'Дизайн',             'sectionUrl' => 'flows/design/'],
+            ['nameSection' => 'Менеджмент' ,        'sectionUrl' => 'flows/management/'],
+            ['nameSection' => 'Маркетинг',          'sectionUrl' => 'flows/marketing/'],
+            ['nameSection' => 'Научпоп' ,           'sectionUrl' => 'flows/popsci/']
         ];
         shuffle($sections);
         return array_slice($sections, 0, 2);
